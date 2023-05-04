b765683d510258af562da4cf6ecf614668b51b5d
uejo <info@taikiuejo.com>
2023-05-04 11:37:13 +0900

    feat：初期の土台構築

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>practice-browsersync</title>
+  <?php wp_head(); ?>
+</head>
+
+<body>
+  <h1>テスト</h1>
+  <?php wp_footer(); ?>
+</body>
+
+</html>
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+// const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+
+module.exports = {
+  entry: {
+    main: "./src/scripts/main.js",
+  },
+  output: {
+    path: path.resolve(__dirname),
+    filename: "scripts/main.bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: "css-loader",
+          },
+        ],
+      },
+      // {
+      //   test: /\.png|\.jpg/,
+      //   type: 'asset/resource',
+      //   generator: {
+      //     filename: "images/[name][ext]",
+      //   },
+      // },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "./styles/main.min.css",
+    }),
+    // new CleanWebpackPlugin(),
+  ],
+};
